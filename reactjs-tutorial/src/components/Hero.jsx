3a1537fea716ce6ce0a9cd5da990f9ca477697a6
Tom Hennessy <tomlhennessy@outlook.com>
2025-05-15 14:39:38 +1000

    hero section

--- reactjs-tutorial/src/components/Hero.jsx
+++ reactjs-tutorial/src/components/Hero.jsx
@@ -1,6 +1,34 @@
 
 export default function Hero() {
+  const name = 'Tom'
+  const weeks = '2,405'
+  const percentage = '45%'
+
   return (
-    <div></div>
+    <section id="hero">
+      <h3 className='text-large'>
+        {name}, you have {weeks} weeks left. Make them count 🫡
+      </h3>
+      <div className='btns-container'>
+        <button>Not {name}?</button>
+        <button className='link-button'>Copy link</button>
+        <button className='link-button'>Reset data</button>
+      </div>
+      <div className='progress-bar'>
+        <div>
+          <div>
+            <i className='fa-solid fa-baby' />
+            <h6 className='bar-label'>Birth</h6>
+          </div>
+          <h6>{percentage}</h6>
+        </div>
+        <div>
+        <div>
+            <i className='fa-solid fa-skull' />
+            <h6 className='bar-label'>Death</h6>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
--- reactjs-tutorial/src/components/layouts/Layout.jsx
+++ reactjs-tutorial/src/components/layouts/Layout.jsx
@@ -3,20 +3,31 @@ export default function Layout(props) {
     const { children } = props
 
     const header = (
-        <header></header>
+        <header>
+            <h1 className='text-medium text-gradient'>
+                Unalive
+            </h1>
+        </header>
     )
 
     const footer = (
-        <footer></footer>
+        <footer>
+            <small>Created by</small>
+            <a target="_blank" href="https://www.linkedin.com/in/tomlhennessy96/">
+                <img alt="pfp" src="https://media.licdn.com/dms/image/v2/D4D03AQFgeqmGk2KFlg/profile-displayphoto-shrink_400_400/B4DZSRP7VeHcAk-/0/1737603659935?e=1752710400&v=beta&t=5q3XeH3eYaiuRFviRuIOrFwQ_1BPQnOa2_Smkdwrk7Y" />
+                <p>@tomlhennessy96</p>
+                <i className="fa-brands fa-linkedin-in"></i>
+            </a>
+        </footer>
     )
 
     return (
-        <div>
+        <>
             {header}
             <main>
                 {children}
             </main>
             {footer}
-        </div>
+        </>
     )
 }
