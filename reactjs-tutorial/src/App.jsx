95ffb19962ceb21cf7c50e9dc1fda9ecf03cb2d2
Tom Hennessy <tomlhennessy@outlook.com>
2025-05-20 10:49:19 +1000

    form inputs

--- reactjs-tutorial/src/App.jsx
+++ reactjs-tutorial/src/App.jsx
@@ -4,6 +4,8 @@ import Clocks from './components/Clocks'
 import Calendar from './components/Calendar'
 import Summary from './components/Summary'
 import { calculateTimeLeft, getLifePercentageLived } from './utils'
+import Portal from './components/Portal'
+import Form from './components/Form'
 
 function App() {
   const birthDate = '1995-06-15'
@@ -11,11 +13,15 @@ function App() {
   const data = calculateTimeLeft(birthDate, lifeExpectancy)
   console.log(data)
   const name = 'Tom'
+  const showModal = true
 
   const percentage = getLifePercentageLived(birthDate, lifeExpectancy)
 
   return (
     <Layout>
+      {showModal && (<Portal>
+        <Form />
+      </Portal>)}
       <Hero name={name} data={data} percentage={percentage}/>
       <Clocks data={data} />
       <Calendar lifeExpectancy={lifeExpectancy} data={data} />
--- reactjs-tutorial/src/components/Form.jsx
+++ reactjs-tutorial/src/components/Form.jsx
@@ -1,5 +1,51 @@
-export default function Form() {
+export default function Form(props) {
+  const { handleCloseModal, handleUpdateData } = props
+
+  const days = Array.from({ length: 31 }, (_, i) => i + 1)
+  const months = [
+      "January", "February", "March", "April", "May", "June",
+      "July", "August", "September", "October", "November", "December"
+  ]
+  const years = Array.from({ length: 100 }, (_, i) => new Date().getFullYear() - i)
+
   return (
-    <div></div>
+    <section id="form">
+      <div>
+        <h4 className='text-gradient'>Your Details</h4>
+        <button onClick={handleCloseModal} className='link-button'>
+          <i className='fa-solid fa-xmark' />
+        </button>
+      </div>
+      <div>
+        <label>Name</label>
+        <input type='text' required />
+      </div>
+      <div>
+        <label>Birthday</label>
+        <div className='bday'>
+          <select>
+            {days.map((d) => {
+              return (
+                <option key={d} value={d}>{d}</option>
+              )
+            })}
+          </select>
+          <select>
+            {months.map((m, i) => {
+              return (
+                <option key={i} value={i + 1}>{m}</option>
+              )
+            })}
+          </select>
+          <select>
+            {years.map((y) => {
+              return (
+                <option key={y} value={y}>{y}</option>
+              )
+            })}
+          </select>
+        </div>
+      </div>
+    </section>
   )
 }
