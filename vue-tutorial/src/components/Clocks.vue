cd3491fb7573ce256108238c3df460f4e995b9af
Tom Hennessy <tomlhennessy@outlook.com>
2025-05-23 13:05:07 +1000

    clocks grid

--- vue-tutorial/src/components/Clocks.vue
+++ vue-tutorial/src/components/Clocks.vue
@@ -10,7 +10,17 @@
 
 <template>
     <section id="clocks">
-        
+        <p><i>Time remaining in different units.</i></p>
+        <div id="clocks-grid">
+            <div class="card clock-card" v-for="(clock, clockIndex) in Object.keys(data)">
+                <div class="circle" :style="{rotate: snapshot[clock] + 'deg'}">
+                    <div class="ticker" :class="{[clock]: true}"></div>
+                </div>
+                <div>
+                    <h5>{{ data[clock] }}</h5>
+                </div>
+            </div>
+        </div>
     </section>
 </template>
 
